286b0da49216c3e6eed40b174d1f87ecf3afe217
Louis <80302893+louislab@users.noreply.github.com>
2021-12-12 18:30:51 +0800

    Initial Commit

---
+++ utlimateToolBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  utlimateToolBox
+//
+//  Created by Louis Lee on 12/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ utlimateToolBox/utlimateToolBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  utlimateToolBoxApp.swift
+//  utlimateToolBox
+//
+//  Created by Louis Lee on 12/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct utlimateToolBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
