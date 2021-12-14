05ed4334e19a63d53b52820668d7883fd88b3622
Louis <80302893+louislab@users.noreply.github.com>
2021-12-15 01:24:11 +0800

    OpenAI (Completion) - Enabled davinci-codex

--- utlimateToolBox/openAI/OpenAIView.swift
+++ utlimateToolBox/openAI/OpenAIView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Alamofire
 
 let apiKey: String = "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"
 
@@ -49,71 +50,64 @@ struct CompletionsView: View {
     @State var inited = false
     @StateObject var settings = Settings()
     
-    func initSettings() {
-        settings.content = "Once upon a time"
-        settings.engine = "davinci"
-        settings.prompt = ""
-        settings.max_tokens = 5
-        settings.temperature = 1
-        settings.top_p = 1
-        settings.n = 1
-        settings.stream = false
-        settings.stop = "\\n"
-        inited = true
-    }
-    
-    func getContent() {
+    func fetchContent() {
         
-        struct RawServerResponse: Decodable {
-            let id: String
-            let object: String
-            let created: Int
-            let model: String
-            let choices: [Choice]
-            
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(apiKey)"
+        ]
+        
+        let parameters: Parameters = [
+            "prompt": "\(settings.prompt)",
+            "max_tokens": settings.max_tokens,
+            "temperature": settings.temperature,
+            "top_p": settings.top_p,
+            "n": settings.n,
+            "stream": settings.stream,
+            "logprobs": NSNull(),
+            "stop": "\(settings.stop)"
+        ]
+        
+        struct ServerResponse: Decodable {
+            let id: String?
+            let object: String?
+            let created: Int?
+            let model: String?
+            let choices: [Choice]?
+            let error: Err?
+
             struct Choice: Decodable {
-                let text: String
-                let index: Int
+                let text: String?
+                let index: Int?
                 let logprobs: JSONNull?
-                let finish_reason: String
+                let finish_reason: String?
+            }
+            
+            struct Err: Decodable {
+                let code: JSONNull?
+                let message: String?
+                let param: JSONNull?
+                let type: String?
             }
         }
         
-        guard let url = URL(string: "https://api.openai.com/v1/engines/\(settings.engine)/completions"),
-            let payload = """
-                {
-                    "prompt": "\(settings.prompt)",
-                    "max_tokens": \(settings.max_tokens),
-                    "temperature": \(settings.temperature),
-                    "top_p": \(settings.top_p),
-                    "n": \(settings.n),
-                    "stream": \(settings.stream),
-                    "logprobs": null,
-                    "stop": "\(settings.stop)"
-                }
-                """.data(using: .utf8) else
-        {
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("Bearer " + apiKey, forHTTPHeaderField: "Authorization")
-        request.httpBody = payload
-        
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard error == nil else { print(error!.localizedDescription); return }
-            guard let jsonData = data else { print("Empty data"); return }
-
-            let serverResponse = try! JSONDecoder().decode(RawServerResponse.self, from: jsonData)
-            
-            let content = serverResponse.choices.first!.text
-            
-            DispatchQueue.main.async {
-                self.settings.content = self.settings.prompt + content
+        AF.request(
+            "https://api.openai.com/v1/engines/\(settings.engine)/completions",
+            method: .post,
+            parameters: parameters,
+            encoding: JSONEncoding.default,
+            headers: headers
+        ).responseDecodable(of: ServerResponse.self) { response in
+            switch response.result {
+            case .success(let value):
+                print("Response: \(String(describing: value.choices?.first?.text))")
+                let content = value.choices?.first?.text ?? ""
+                settings.content = "\(settings.prompt)\(content)"
+            case .failure(let error):
+                debugPrint("Error: \(error)")
             }
-        }.resume()
+        }
+        
     }
     
     var body: some View {
@@ -134,27 +128,44 @@ struct CompletionsView: View {
                     .buttonStyle(MonoStyle_s())
             }
             TextEditor(text: $settings.content)
-                .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
+                .font(Font.custom("RobotoMono-Light", size: 15))
+                .disableAutocorrection(true)
                 .padding()
                 .modifier(InnerShadowModifier())
                 .padding([.leading, .bottom, .trailing])
                 .onAppear {
                     if inited == false {
-                        initSettings()
+                        settings.initSettings()
+                        inited = true
                     }
                 }
-            Button(action: {
-                settings.prompt = settings.content
-                getContent()
-                hideKeyboard()
-            }) {
-                Text("Generate")
-                    .fontWeight(.bold)
+            HStack {
+                Spacer()
+                Button(action: {
+                    settings.content = ""
+                }) {
+                    Text("Clear")
+                        .fontWeight(.bold)
+                }
+                    .buttonStyle(MonoStyle())
+                Spacer()
+                Button(action: {
+                    settings.prompt = settings.content
+                    fetchContent()
+                    hideKeyboard()
+                }) {
+                    Text("Generate")
+                        .fontWeight(.bold)
+                }
+                    .buttonStyle(MonoStyle())
+                Spacer()
             }
-            .buttonStyle(MonoStyle())
         }
         .navigationTitle("Completions")
         .navigationBarTitleDisplayMode(.inline)
+        .onDisappear(perform: {
+            settings.saveSettings()
+        })
     }
 }
 
@@ -244,6 +255,21 @@ struct CompletionsView_menu: View {
                         .padding([.leading, .bottom, .trailing])
                         .frame(width: 220.0, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
                 }
+                Button(action: {
+                    settings.content = "Once upon a time"
+                    settings.engine = "davinci"
+                    settings.prompt = ""
+                    settings.max_tokens = 10
+                    settings.temperature = 1
+                    settings.top_p = 1
+                    settings.n = 1
+                    settings.stream = false
+                    settings.stop = "\\n"
+                },
+                       label: {
+                    Text("Reset")
+                })
+                    .padding()
             }
         }
         .navigationTitle("Configuration")
@@ -251,10 +277,13 @@ struct CompletionsView_menu: View {
         .onTapGesture {
             hideKeyboard()
         }
+        .onDisappear(perform: {
+            settings.saveSettings()
+        })
     }
 }
 
-// MARK: - Shared settings
+// MARK: - Completion settings class
 
 class Settings: ObservableObject {
     @Published var content = ""
@@ -275,6 +304,79 @@ class Settings: ObservableObject {
             //rounds the Double to an Int
             self.max_tokens = Int($0)
         })
+    }
+    
+    // used for data saving, since codable and observable cannot stand together
+    struct CompletionModel: Codable {
+        var content: String
+        var engine: String
+        var prompt: String
+        var max_tokens: Int
+        var temperature: Double
+        var top_p: Double
+        var n: Int
+        var stream: Bool
+        var stop: String
+    }
+    
+    func initSettings() {
+        
+        let reader = FilesManager()
+        var rdata: Data
+        do {
+            try rdata = reader.read(fileNamed: "completion_model_settings.json")
+        } catch {
+            print(error)
+            self.content = "Once upon a time"
+            self.engine = "davinci"
+            self.prompt = ""
+            self.max_tokens = 10
+            self.temperature = 1
+            self.top_p = 1
+            self.n = 1
+            self.stream = false
+            self.stop = "\\n"
+            print("Data loaded from template.")
+            return
+        }
+        
+        let data = try! JSONDecoder().decode(CompletionModel.self, from: rdata)
+        
+        self.content = data.content
+        self.engine = data.engine
+        self.prompt = data.prompt
+        self.max_tokens = data.max_tokens
+        self.temperature = data.temperature
+        self.top_p = data.top_p
+        self.n = data.n
+        self.stream = data.stream
+        self.stop = data.stop
+        
+    }
+    
+    func saveSettings() {
+        
+        let writer = FilesManager()
+        let wdata = CompletionModel(
+            content: self.content,
+            engine: self.engine,
+            prompt: self.prompt,
+            max_tokens: self.max_tokens,
+            temperature: self.temperature,
+            top_p: self.top_p,
+            n: self.n,
+            stream: self.stream,
+            stop: self.stop
+        )
+        
+        let data = try! JSONEncoder().encode(wdata)
+        
+        do {
+            try writer.save(fileNamed: "completion_model_settings.json", data: data)
+        } catch {
+            print(error)
+        }
+        
     }
 }
 
@@ -373,7 +475,7 @@ struct InnerShadowModifier: ViewModifier {
         }
 }
 
-// MARK: - Encode/decode helpers
+// MARK: - JSONNull encode/decode helper
 
 class JSONNull: Codable, Hashable {
 
@@ -404,8 +506,69 @@ class JSONNull: Codable, Hashable {
     }
 }
 
+// MARK: - Modded fileManager for save / read data
+
+class FilesManager {
+    
+    enum Error: Swift.Error {
+        case fileAlreadyExists // not used
+        case invalidDirectory
+        case writtingFailed
+        case fileNotExists
+        case readingFailed
+    }
+    
+    // create a real fileManage
+    let fileManager: FileManager
+    
+    init(fileManager: FileManager = .default) {
+        self.fileManager = fileManager
+    }
+    
+    // streamlined processing, enhanced error handling
+    func save(fileNamed: String, data: Data) throws {
+        guard let url = makeURL(forFileNamed: fileNamed) else {
+            throw Error.invalidDirectory
+        }
+        do {
+            try data.write(to: url)
+            print("Data saved to local storage.")
+        } catch {
+            debugPrint(error)
+            throw Error.writtingFailed
+        }
+    }
+    
+    func read(fileNamed: String) throws -> Data {
+        guard let url = makeURL(forFileNamed: fileNamed) else {
+            throw Error.invalidDirectory
+        }
+        guard fileManager.fileExists(atPath: url.path) else {
+            throw Error.fileNotExists
+        }
+        do {
+            let result = try Data(contentsOf: url)
+            print("Data loaded from local storage.")
+            return result
+        } catch {
+            debugPrint(error)
+            throw Error.readingFailed
+        }
+    }
+    
+    // sub-function for file name processing
+    private func makeURL(forFileNamed fileName: String) -> URL? {
+        guard let url = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return url.appendingPathComponent(fileName)
+    }
+    
+}
+
 // MARK: - Extensions
 
+// option to dismiss the keyboard when user tapped specific area
 extension View {
     func hideKeyboard() {
         let resign = #selector(UIResponder.resignFirstResponder)
